a5cb913326cdde1d0ecdab0a4d7cb443048444a4
nakyeonko3 <badaclock@gmail.com>
2023-03-16 15:28:38 +0900

    update

--- createcsvfile.py
+++ createcsvfile.py
@@ -3,3 +3,8 @@ import pandas as pd
 def create_csv_by_table_list(data_table): # 받은 리스트를 csv파일로 바꿔주는 함수
     df = pd.DataFrame(data_table, columns=['Disease', 'Farm', 'Location', 'Date', 'Animal', 'Number', 'Diagnosis', 'End_date'])
     df.to_csv('diseases.csv', index=False)
+    
+
+def create_extra_csv_by_table_list(data_table): # 받은 리스트를 csv파일로 바꿔주는 함수
+    df = pd.DataFrame(data_table, columns=['Disease', 'Farm', 'Location', 'Date', 'Animal', 'Number', 'Diagnosis', 'End_date'])
+    df.to_csv('extra_csv_diseases.csv', index=False)
--- initcrawler.py
+++ initcrawler.py
@@ -6,7 +6,7 @@ from movepage import check_how_many_number_buttons, move_to_page_by_number_butto
 from showcurrentpagenumber import show_cuttent_page_number
 from checkPage import checkIsLastPage, showLastPageNumber
 from readpagetable import create_data_table_list
-from createcsvfile import create_csv_by_table_list
+from createcsvfile import create_csv_by_table_list, create_extra_csv_by_table_list
 
 driver = webdriver.Chrome(executable_path='chromedriver') # 드라이버 인스턴스 생성
 URL='https://home.kahis.go.kr/home/lkntscrinfo/selectLkntsOccrrncList.do'
@@ -38,10 +38,9 @@ init(driver)
 number_buttons = driver.find_elements(By.XPATH," /html/body/div[1]/div[2]/div[3]/form[2]/table[5]/tbody/tr[2]/td/table/tbody/tr/td[contains(@style,'padding-top:1px')]")
 LastPageNumber = showLastPageNumber(driver=driver)
 
-LastPageNumber = 30
-
 executioncount = 0
 executionLimit = LastPageNumber / 10 -1
+# executionLimit = 0
 
 # while not checkIsLastPage(driver = driver):
 while True:
@@ -65,10 +64,26 @@ while True:
     
     executioncount = executioncount + 1
     print(f"executioncount: {executioncount}")
-    print(f"if_true?: {executioncount>executionLimit}")
     if executioncount > executionLimit:
         break
     
+    
+        last_number = check_how_many_number_buttons(driver=driver) - 1
+    current_number = 0
+    create_data_table_list(driver=driver, data_table=data_table)
 create_csv_by_table_list(data_table=data_table)
+
+if LastPageNumber%10 > 0:
+    last_number = check_how_many_number_buttons(driver=driver) - 1
+    current_number = 0
+    create_data_table_list(driver=driver, data_table=data_table)
+    
+    while current_number < last_number:
+        current_number = current_number + 1
+        number_buttons = driver.find_elements(By.XPATH," /html/body/div[1]/div[2]/div[3]/form[2]/table[5]/tbody/tr[2]/td/table/tbody/tr/td[contains(@style,'padding-top:1px')]")
+        number_buttons[current_number].click()
+        create_data_table_list(driver=driver, data_table=data_table)
+        
+create_extra_csv_by_table_list(data_table=data_table)
 
 
